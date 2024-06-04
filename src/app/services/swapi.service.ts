a3772af9dfcace9f69e3b691eeea1330a45ccb00
Mateusz Zmuda <mateusz.zmuda3@gmail.com>
2024-06-04 15:44:43 +0200

    Implemented  methods to fetch people and starships

--- src/app/services/swapi.service.ts
+++ src/app/services/swapi.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SwapiService {
+  private baseUrl = 'https://www.swapi.tech/api';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getRandomPerson(): Observable<any> {
+    const id = Math.floor(Math.random() * 83) + 1; // SWAPI has 83 people
+    return this.http.get(`${this.baseUrl}/people/${id}`);
+  }
+
+  getRandomStarship(): Observable<any> {
+    const id = Math.floor(Math.random() * 36) + 1; // SWAPI has 36 starships
+    return this.http.get(`${this.baseUrl}/starships/${id}`);
+  }
 }
